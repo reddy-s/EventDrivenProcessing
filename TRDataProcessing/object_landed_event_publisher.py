6d4cddfb7a431bd5b888a6d3c9a5229bb8aa63a8
Sangram Reddy <reddy.horcrux@gmail.com>
2022-07-01 09:40:17 +0100

    Event Bridge: Infra and Publishing related code

--- TRDataProcessing/object_landed_event_publisher.py
+++ TRDataProcessing/object_landed_event_publisher.py
@@ -1,24 +1,39 @@
 import json
+import boto3
+
+from uuid import uuid4
 
 
 def fun(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    records = event.get("Records")
+    client = boto3.client('events')
+    publish_status = []
+    for record in records:
+        s3_payload = record.get("s3")
+        correlation_id = str(uuid4())
+        payload = {
+            "correllationId": correlation_id,
+            "s3": s3_payload
+        }
+        status = client.put_events(
+            Entries=[
+                {
+                    'Source': 'ObjectLandedEventPublisher',
+                    'DetailType': 'ObjectLandedEvent',
+                    'Detail': json.dumps(payload),
+                    'EventBusName': 'MLPlatformDataArrivedEventBus',
+                    'TraceHeader': correlation_id
+                },
+            ]
+        )
+        publish_status.append(status)
+    print(event)
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(publish_status)
     }
+    print(response)
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
